5e681a804ffdc7ac590f2755855b1bc899335e48
Enver GÖKMEN <envergokmen@gmail.com>
2023-11-21 17:25:58 +0300

    log Android file name for debug purpose

--- main.sh
+++ main.sh
@@ -4,6 +4,18 @@
   export LANGUAGE=en_US.UTF-8
   locale
   curl -o "./$AndroidFileName" -k $AndroidFileUrl
+  ls -lh
+
+file_path="./$AndroidFileName"
+
+  if [ -e "$file_path" ]; then
+    file_size=$(du -h "$file_path" | cut -f1)
+    echo "File exists: $file_path"
+    echo "File size: $file_size"
+else
+    echo "File does not exist: $file_path"
+fi
+
   bundle init
   echo "gem \"fastlane\"">>Gemfile
   bundle install
@@ -13,6 +25,9 @@
   mv "$FastFileConfig" "fastlane/Fastfile"
   mv "$AppFileConfig" "fastlane/Appfile"
   mv "$ApiKey" "$ApiKeyFileName"
+
+  ls -lh fastlane
+  
   bundle exec fastlane $FastlaneParams --verbose
   if [ $? -eq 0 ]
   then
