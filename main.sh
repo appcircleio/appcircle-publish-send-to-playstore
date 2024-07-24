337b7221cfd53108b6a62befba011761a09f1dbe
Enver GÖKMEN <envergokmen@gmail.com>
2024-07-24 13:22:13 +0300

    BE-4093 env name convention applied

--- main.sh
+++ main.sh
@@ -3,10 +3,10 @@
   export LANG=en_US.UTF-8
   export LANGUAGE=en_US.UTF-8
   locale
-  curl -o "./$AndroidFileName" -k $AndroidFileUrl
+  curl -o "./$AC_APP_FILE_NAME" -k $AC_APP_FILE_URL
   ls -lh
 
-file_path="./$AndroidFileName"
+file_path="./$AC_APP_FILE_NAME"
 
   if [ -e "$file_path" ]; then
     file_size=$(du -h "$file_path" | cut -f1)
@@ -23,19 +23,19 @@ fi
   touch fastlane/Appfile
   touch fastlane/Fastfile
    
-  if [ -f "$ReleaseNotes" ]; then
+  if [ -f "$AC_RELEASE_NOTES_FILE" ]; then
     echo "Found change log in AC_RELEASE_NOTES, copying to fastlane/metadata/android/en-us/changelogs/default.txt"
     mkdir -p "fastlane/metadata/android/en-us/changelogs"
-    cp "$ReleaseNotes" "fastlane/metadata/android/en-us/changelogs/default.txt"
+    cp "$AC_RELEASE_NOTES_FILE" "fastlane/metadata/android/en-us/changelogs/default.txt"
   else
-    echo "Warning: AC_RELEASE_NOTES is not found, changelog will be skipped."
+    echo "Warning: AC_RELEASE_NOTES_FILE is not found, changelog will be skipped."
   fi
 
-  mv "$FastFileConfig" "fastlane/Fastfile"
-  mv "$AppFileConfig" "fastlane/Appfile"
-  mv "$ApiKey" "$ApiKeyFileName"
+  mv "$AC_FASTFILE_CONFIG" "fastlane/Fastfile"
+  mv "$AC_APP_FILE_CONFIG" "fastlane/Appfile"
+  mv "$AC_API_KEY" "$AC_API_KEY_FILE_NAME"
   
-  bundle exec fastlane $FastlaneParams --verbose
+  bundle exec fastlane $AC_FASTLANE_PARAMS --verbose
   if [ $? -eq 0 ]
   then
   echo "PlayStore progress succeeded"
