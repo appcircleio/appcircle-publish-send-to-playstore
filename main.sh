495d647ebb74dab2a8c1a3e9d644a03a8bc3803a
burak.yildirim <burak_yildirim_official@hotmail.com>
2024-08-14 10:45:15 +0300

    added logic for apk/aab upload

--- main.sh
+++ main.sh
@@ -27,7 +27,14 @@ fi
   mv "$AC_APP_FILE_CONFIG" "fastlane/Appfile"
   mv "$AC_API_KEY" "$AC_API_KEY_FILE_NAME"
   
-  bundle exec fastlane $AC_FASTLANE_PARAMS --verbose
+  if [[ "$AC_BINARY_FILE_TYPE" == "apk" ]] ; then
+      bundle exec fastlane upload_apk --verbose
+  fi
+
+  if [[ "$AC_BINARY_FILE_TYPE" == "aab" ]] ; then
+      bundle exec fastlane upload_aab --verbose
+  fi
+  
   if [ $? -eq 0 ]
   then
   echo "Google Play Console progress succeeded"
